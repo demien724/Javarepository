efa47887230650378d7dd333cc80eead8cfce457
demien724 <124111115+demien724@users.noreply.github.com>
2023-02-06 18:09:38 +0900

    Add files via upload

---
+++ src/과일/사과.java
@@ -0,0 +1,9 @@
+package 과일;
+
+public class 사과 {
+
+	public static void main(String[] args) {
+		System.out.print("이것은 사과입니다.");
+	}
+
+}
---
+++ src/제어문/문제풀이3.java
@@ -0,0 +1,18 @@
+package 제어문;
+
+public class 문제풀이3 {
+
+	public static void main(String[] args) {
+//		입력
+		double x = 33.2;
+		double y = 22.2;
+
+//		처리 + 출력
+		System.out.println(x == 44.2);
+		System.out.println(x == y);
+		System.out.println(y > 55);
+		
+		
+	}
+
+}
