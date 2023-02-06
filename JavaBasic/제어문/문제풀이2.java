447255ba2b288af272e294c2beac746906095487
demien724 <124111115+demien724@users.noreply.github.com>
2023-02-06 18:17:55 +0900

    Add files via upload

---
+++ JavaBasic/제어문/문제풀이2.java
@@ -0,0 +1,19 @@
+package 제어문;
+
+public class 문제풀이2 {
+
+	public static void main(String[] args) {
+//		입력
+		int num1 = 30;
+		int num2 = 40;
+		
+//		처리
+		int result1 = num1/num2;
+		int result2 = num1%num2;
+		
+//		출력
+		System.out.println("num1 에서 num2를 나누면 " + result1);
+		System.out.println("num1 에서 num2를 나눈 나머지는 " + result2);
+	}
+
+}
