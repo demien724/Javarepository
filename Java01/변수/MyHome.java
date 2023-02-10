c0cc35044a2af4049d49895d03ac11d2c5c334b1
demien724 <124111115+demien724@users.noreply.github.com>
2023-02-10 17:54:05 +0900

    Add files via upload

---
+++ Java01/변수/MyHome.java
@@ -0,0 +1,19 @@
+package 변수;
+
+public class MyHome {
+
+	public static void main(String[] args) {
+		int myAge = 100;
+
+//		전화번호
+		String myTel = "01011113333";
+
+		String ssn = "980321";
+
+// 		절대 값을 변하지 않게 해줘
+//		상수 변수 : final, 변수명을 전부 대문자로 만들자! 
+		final double PI = 3.14;
+
+	}
+
+}
